36ea14f8261959a67b78e58d2b968b207fd19db0
sion-1122 <sionsaiyan.com>
2024-12-08 08:37:56 -0800

    fixed

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -10,6 +10,8 @@ const page = () => {
       <h1 className='text-2xl text-center font-bold my-3'>Related Products</h1>
       <div className="grid grid-cols-1 p-10 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
         {products.map((product) => (
+          // @ts-ignore
+          
           <ProductCard product={product} />
         ))}
       </div>
