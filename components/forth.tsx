49e7a3721845bd412a61aea07c7f47768385c2f2
sion-1122 <sionsaiyan.com>
2024-12-08 08:40:43 -0800

    fix

--- components/forth.tsx
+++ components/forth.tsx
@@ -51,6 +51,7 @@ const forth = () => {
       </h1>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
         {products.map((product) => (
+          // @ts-ignore
           <ProductCard product={product} />
         ))}
       </div>
